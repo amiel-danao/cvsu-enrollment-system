09c7d2f25b2ef190367935e4a18717258bcfd5aa
Amiel Danao <amielrenaissance4@gmail.com>
2022-11-10 02:11:15 +0800

    updated settings.py to remove env variable related to email notif

--- enrollmentsystem/settings.py
+++ enrollmentsystem/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 from django.contrib.messages import constants as messages
 import os
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -191,7 +187,7 @@ EMAIL_HOST_USER = '21stfloordev@gmail.com'
 EMAIL_HOST_PASSWORD = 'fuuiyivhsndlgygt'
 
 # Custom setting. To email
-RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
+# RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
 
 
 # if os.getenv('GAE_APPLICATION', None):
